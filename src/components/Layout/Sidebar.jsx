d662c0697067fb0c907ec37db8e9c3850e945f6f
DNishadini <dinushikaherath150@gmail.com>
2025-12-29 11:40:32 +0530

    create sidebar

---
+++ src/components/Layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+  return (<div className="transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relativez-10">
+    <div classname>
+
+    </div>
+  </div>
+  );
+}
+export default Sidebar;
