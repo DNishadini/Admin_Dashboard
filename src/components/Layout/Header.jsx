62eba143d061e2be0dded8f386b458eb257b125d
DNishadini <dinushikaherath150@gmail.com>
2025-12-29 21:33:27 +0530

    Add the icons

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu, Search } from "lucide-react";
+import { Filter, Menu, Search, Plus, Sun, Bell, Settings } from "lucide-react";
 
 function Header() {
   return (
@@ -27,7 +27,28 @@ function Header() {
               placeholder="Search Anything"
               className="w-full pl-10 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-slate-200  dark:border-slate-700 rounded-xl text-slate-800 dark:text-white placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
             />
+            <button className="absolute right-2 top-1/2 transform -translate-y-1/2 p-1.5 text-slate-400 hover:text-slate-600 dark:hover:text-slate-300">
+              <Filter />
+            </button>
           </div>
+        </div>
+        <div className="flex items-center space-x-3">
+          <button className="hidden lg:flex items-center space-x-2 py-2 px-4 bg-gradient-to-r from-blue-500 to-purple-600 text-whiterounded-xl hover:shadow-lg transition-all">
+            <Plus className="w-4 h-4" />
+            <span className="text-sm font-medium">New</span>
+          </button>
+          <button className="p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
+            <Sun className="w-5 h-5" />
+          </button>
+          <button className="relative p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-800 transition-colors">
+            <Bell className="w-5 h-5" />
+            <span className="absolute -top-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
+              3
+            </span>
+          </button>
+          <button className="p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
+            <Settings />
+          </button>
         </div>
       </div>
     </div>
