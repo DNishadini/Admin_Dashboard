c0f34bb4bef38ef105388ff327abf62c54868833
DNishadini <dinushikaherath150@gmail.com>
2026-01-02 22:33:31 +0530

    create Revenue Chart

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import StatsGrid from "./StatsGrid";
+import ChartSection from "./ChartSection";
 
 function Dashboard() {
   return (
     <div className="space-y-6">
       <StatsGrid />
+      <ChartSection />
     </div>
   );
 }
