5f8837e85d457717a4c6a74b8ac8364b4e46035a
DNishadini <dinushikaherath150@gmail.com>
2026-01-05 00:08:13 +0530

    Fully complete TableSection

--- src/components/Dashboard/TableSection.jsx
+++ src/components/Dashboard/TableSection.jsx
@@ -1,4 +1,4 @@
-import { MoreHorizontal, TrendingUp } from "lucide-react";
+import { MoreHorizontal, TrendingDown, TrendingUp } from "lucide-react";
 
 const recentOrders = [
   {
@@ -185,25 +185,41 @@ function TableSection() {
         </div>
         {/*Dynamic Data*/}
         <div className="p-6 space-y-4">
-          <div className="flex items-center justify-between p-4 rounded-xl hover:bg-slate-50 dark:hover:bg-slate-800/50 transition-colors">
-            <div className="flex-1">
-              <h4 className="text-sm font-semibold text-slate-800 dark:text-white">
-                Product Name
-              </h4>
-              <p className="text-xs text-slate-500 dark:text-slate-400">
-                Product Sales
-              </p>
-            </div>
-            <div className="text-right">
-              <p className="text-sm font-semibold text-slate-800 dark:text-white">
-                Product Revenue
-              </p>
-              <div className="flex items-center space-x-1">
-                <TrendingUp className="w-3 h-3 text-emerald-500" />
-                <span>Product Change</span>
+          {topProducts.map((product, index) => {
+            return (
+              <div className="flex items-center justify-between p-4 rounded-xl hover:bg-slate-50 dark:hover:bg-slate-800/50 transition-colors">
+                <div className="flex-1">
+                  <h4 className="text-sm font-semibold text-slate-800 dark:text-white">
+                    {product.name}
+                  </h4>
+                  <p className="text-xs text-slate-500 dark:text-slate-400">
+                    {product.sales}
+                  </p>
+                </div>
+                <div className="text-right">
+                  <p className="text-sm font-semibold text-slate-800 dark:text-white">
+                    {product.revenue}
+                  </p>
+                  <div className="flex items-center space-x-1">
+                    {product.trend === "up" ? (
+                      <TrendingUp className="w-3 h-3 text-emerald-500" />
+                    ) : (
+                      <TrendingDown className="w-3 h-3 text-red-500" />
+                    )}
+                    <span
+                      className={`text-xs font-medium ${
+                        product.trend === "up"
+                          ? "text-emerald-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {product.change}
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
